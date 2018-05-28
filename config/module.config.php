caeb9bbdd730d7adb400cc74194226ef86b408dc
Masterflash <sxq@yandex.ru>
2018-05-28 09:48:03 +0300

    f

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,7 @@
  */
 
 namespace Mf\Navigation;
-
+use Zend\ServiceManager\Factory\InvokableFactory;
 
 return [
   /*конфиг по умолчанию, создайте аналогичный ключ в глобальном конфиге с нужными типами меню*/
@@ -13,13 +13,25 @@ return [
 
     'view_helpers' => [
         'factories' => [
-            View\Helper\Menu::class => View\Helper\Factory\Menu::class,
+            /*Рабочие помощники*/
+            View\Helper\Menu::class => View\Helper\Factory\HelperFactory::class,
+            View\Helper\Breadcrumbs::class => View\Helper\Factory\HelperFactory::class,
+            View\Helper\Pagination::class => InvokableFactory::class,
+            
+            /*технологические помощники, которые собственно генерируют номера страниц*/
+            View\Helper\Simba\Pagination::class => InvokableFactory::class,
+            View\Helper\Bootstrap4\Pagination::class => InvokableFactory::class,
+            /*Подменный прокси navigation для не ZF3 генераторов ссылок*/
             Service\Bootstrap4Navigation::class => Service\NavigationFactory::class,
         ],
         'aliases' => [
             'Menu' => View\Helper\Menu::class,
             'menu' => View\Helper\Menu::class,
+            'Breadcrumbs' =>View\Helper\Breadcrumbs::class,
+            'breadcrumbs' =>View\Helper\Breadcrumbs::class,
             "bootstrap4Navigation" => Service\Bootstrap4Navigation::class,
+            'Pagination' => View\Helper\Pagination::class,
+            'pagination' => View\Helper\Pagination::class,
         ],
     ],
 ];
