03fbebca3ced76d8a17551bc6ebebfca2c80760e
Masterflash <sxq@yandex.ru>
2019-05-23 16:26:58 +0300

    Update GetMvcUrls.php

--- src/Service/Admin/JqGrid/Plugin/GetMvcUrls.php
+++ src/Service/Admin/JqGrid/Plugin/GetMvcUrls.php
@@ -49,6 +49,7 @@ class GetMvcUrls extends AbstractPlugin
     * $colModel - элемент $colModel из конфигурации
     * $toolbarData - массив начальных данных из раздела toolbar
     * возвращает тот же $colModel, с внесенными изменениями
+    * заносит ВСЕ элменты MVC сайта на всех языках
     */
     public function colModel(array $colModel,array $toolbarData=[])
     {
@@ -57,9 +58,11 @@ class GetMvcUrls extends AbstractPlugin
             //внутри контроллера
             if (is_array($desc)) {
                 foreach ($desc as $meta) {//цикл по отдельным модулям
-                    if (isset($meta["urls"]["mvc"][$toolbarData["locale"]])){
-                        foreach ($meta["urls"]["mvc"][$toolbarData["locale"]] as $k=>$item){
-                            $rez[$item]=$meta["description"].' -> '.$meta["urls"]["name"][$toolbarData["locale"]][$k];
+                    if (is_array($meta["urls"]["mvc"])){
+                        foreach ($meta["urls"]["mvc"] as $locale=>$allitem){
+                            foreach ($allitem as $k=>$item){
+                                $rez[$item]=$meta["description"].' -> '.$meta["urls"]["name"][$locale][$k];
+                            }
                         }
                     }
                 }
